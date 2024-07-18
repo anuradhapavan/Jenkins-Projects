0332783469550cffeeecde9bd7440107605b2afd
Pavan Kumar Janyavula <pavanjanyavula.etl@gmail.com>
2024-07-18 14:42:16 -0500

    Second Project

---
+++ SeleniumLambdaDemo/src/test/java/lambaDemo/Browser.java
@@ -0,0 +1,15 @@
+package lambaDemo;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class Browser {
+
+	
+	@Test
+	public void myTest() {
+		System.out.println("Ran Tests");
+		assertEquals(true, true);
+	}
+}
